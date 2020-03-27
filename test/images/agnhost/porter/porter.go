c2f1cea5ef2abcdfa10fb4f808fc515fb9d1ef2d
Federico Paolinelli <fpaoline@redhat.com>
2020-03-27 12:44:02 +0100

    Add sctp support to the agnhost porter command.
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- test/images/agnhost/porter/porter.go
+++ test/images/agnhost/porter/porter.go
@@ -29,10 +29,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/ishidawataru/sctp"
 	"github.com/spf13/cobra"
 )
 
 const prefix = "SERVE_PORT_"
+const sctpPrefix = "SCTP_SERVE_PORT_"
 const tlsPrefix = "SERVE_TLS_PORT_"
 
 // CmdPorter is used by agnhost Cobra.
@@ -45,7 +47,9 @@ Additionally, if the environment variable "SERVE_TLS_PORT_9002" is set, then the
 
 The included "localhost.crt" is a PEM-encoded TLS cert with SAN IPs "127.0.0.1" and "[::1]", expiring in January 2084, generated from "src/crypto/tls".
 
-To use a different cert/key, mount them into the pod and set the "CERT_FILE" and "KEY_FILE" environment variables to the desired paths.`,
+To use a different cert/key, mount them into the pod and set the "CERT_FILE" and "KEY_FILE" environment variables to the desired paths.
+
+The server also supports serving the requested data on SCTP ports. To enable serving on SCTP ports, the environment variable must start with SCTP_. For example, an environment like "SCTP_SERVE_PORT_9001 will make the subcommand to serve via SCTP on port 9001"`,
 	Args: cobra.MaximumNArgs(0),
 	Run:  main,
 }
@@ -65,6 +69,10 @@ func main(cmd *cobra.Command, args []string) {
 		if strings.HasPrefix(key, tlsPrefix) {
 			port := strings.TrimPrefix(key, tlsPrefix)
 			go serveTLSPort(port, value)
+		}
+		if strings.HasPrefix(key, sctpPrefix) {
+			port := strings.TrimPrefix(key, sctpPrefix)
+			go serveSCTPPort(port, value)
 		}
 	}
 
@@ -97,4 +105,39 @@ func serveTLSPort(port, value string) {
 		keyFile = "localhost.key"
 	}
 	log.Printf("tls server on port %q with certFile=%q, keyFile=%q failed: %v", port, certFile, keyFile, s.ListenAndServeTLS(certFile, keyFile))
+}
+
+func serveSCTPPort(port, value string) {
+	serverAddress, err := sctp.ResolveSCTPAddr("sctp", "0.0.0.0:"+port)
+	if err != nil {
+		log.Fatal("Sctp: failed to resolve address. error:", err)
+	}
+
+	listener, err := sctp.ListenSCTP("sctp", serverAddress)
+	if err != nil {
+		log.Fatal("Failed to listen SCTP. error:", err)
+	}
+	log.Printf("Started SCTP server")
+
+	defer listener.Close()
+	defer func() {
+		log.Printf("SCTP server exited")
+	}()
+
+	for {
+		conn, err := listener.AcceptSCTP()
+		if err != nil {
+			log.Fatal("Failed to accept SCTP. error:", err)
+		}
+		go func(conn *sctp.SCTPConn) {
+			log.Println("Sending response")
+			_, err = conn.Write([]byte(value))
+			if err != nil {
+				log.Println("Failed to send response", err)
+				return
+			}
+			log.Println("Response sent")
+		}(conn)
+	}
+
 }
